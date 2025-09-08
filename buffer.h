c60c09a0831d632ac90b4d8ea282880e5a3cfd0b
SidneySJk <125586766+SidneySJk@users.noreply.github.com>
2025-09-07 20:54:31 -0500

    Se actualizo la libreria buffer
    Se cambio la funcion espacioDeMemoria, ahora no necesita de una entrada para guardar memoria  de una cadena

--- buffer.h
+++ buffer.h
@@ -1,28 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #ifndef BUFFER_H
 #define BUFFER_H
 
 // Lee la cadena caracter por caracter para determinar su espacio de memoria
-char* espacioDeMemoria(char* cadena) {
+char* espacioDeMemoria() {
     
-    //char* capacidad = NULL;
+    char* cadena = NULL;
     int caracteres = 0;
+    int c;
+    
+    char* capacidad = (char*)malloc((caracteres+1) * sizeof(char));
+    c = getchar();
 
-    while (cadena[caracteres] != '\n' && cadena[caracteres] != '\0') {
+    while (( c = getchar()) != '\n' && c != EOF) { //Fuente: https://rabbit.eng.miami.edu/class/een218/getchar.html
+        capacidad = realloc(cadena,( caracteres+2) * sizeof(char));//Se suma 2 para que quepa el caracter y '/0'
+        if (capacidad == NULL) {
+            printf("Error en la asignacion de memoria\n");
+            free(capacidad);
+            exit(1);
+        }
+        cadena = capacidad;
+        cadena[caracteres] = (char)c;//'\0'
         caracteres+=1;
     }
-    char* capacidad = (char*)malloc((caracteres+1) * sizeof(char));
-    if (capacidad == NULL) {
-        printf("Error en la asignacion de memoria\n");
-        //free(capacidad);
-        exit(1);
+   
+    if (cadena != NULL) {
+        cadena[caracteres] = '\0';
     }
+    //capacidad[caracteres] = '\0';
 
-    strncpy(capacidad, cadena, caracteres);
-    capacidad[caracteres] = '\0';
-    return capacidad;
+    //return capacidad;
+    return cadena;
 }
 
 #endif
