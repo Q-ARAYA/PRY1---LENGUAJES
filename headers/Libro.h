98bdcc5de9d1c46762da18a318310fbc2d47f193
Jareck A. Levell Cordero <88910470+Jareck-Levell@users.noreply.github.com>
2025-09-13 22:19:14 -0600

    Movimiento de los headers
    - Se mueven Cliente.h y Libro.h
    - Se reestructuran  Cliente.h y Libro.h para su correcto funcionamiento

---
+++ headers/Libro.h
@@ -0,0 +1,27 @@
+#ifndef LIBRO_H
+#define LIBRO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Macros para configuración
+#define ARCHIVO_LIBROS "data/Libros.txt"
+#define MAX_NOMBRE 100
+
+// Definición del struct
+typedef struct {
+    char *nombreLibro;
+    float *precioLibro;
+    int codigoLibro;
+    int *cantidad;
+} Libro;
+
+// Prototipos de funciones
+Libro CrearLibro();
+int obtenerSiguienteCodigo();
+void registrarLibro();
+void guardarLibroEnTXT(Libro *lib);
+void LiberarMemoriaLibro(Libro *lib);
+void inventarioDePrograma();
+
+#endif
